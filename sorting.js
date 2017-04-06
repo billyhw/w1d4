b853db5e05566cd8422f57dac62e2d015a7b000d
Billy Chang <billyheungwing@yahoo.ca>
2017-04-06 17:11:21 +0000

    sorting.js

---
+++ sorting.js
@@ -0,0 +1,28 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+// sort by value
+var numericSort = function (a, b) {
+  return b.age - a.age;
+};
+
+// sort by name
+var mySort = function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  // names must be equal
+  return numericSort(a, b);
+};
+
+console.log(students.sort(mySort));
