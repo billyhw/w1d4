4fa3be651d6cd288af8acad86eb6b540217d91fc
Billy Chang <billyheungwing@yahoo.ca>
2017-04-06 21:23:02 +0000

    added wrapLog.js

---
+++ wrapLog.js
@@ -0,0 +1,15 @@
+var wrapLog = function (callback, name) {
+
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    var result = callback.apply(null, arguments);
+    console.log(name + "(" + args + ") => " + result);
+  }
+
+};
+
+var sum = function (a,b,c) { return a + b + c; };
+var logSum = wrapLog(sum, "sum");
+
+logSum(5,3,2); // sum(5,3) => 8
+logSum(3,2,1); // sum(3,2) => 5
